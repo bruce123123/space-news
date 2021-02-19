ada65f550e400548ac8d0486f91b5cfc4ffbb46b
bruce-h <bdhay@protonmail.com>
2021-02-19 16:36:03 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Route, Link} from 'react-router-dom';
+import axios from 'axios';
+import parkData from './data/parks.json';
+import ParkList from './ParkList';
+import ParkDetail from './ParkDetail';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      parkList:[]
+    }
+  }
+
+
+  componentDidMount = async () => {
+    const parkData = await axios.get("https://spaceflightnewsapi.net/api/v2/articles");
+    this.setState({
+      parkList: parkData.data.data
+    })
+  
+  }
+
+  render() {
+    return (
+        <div>
+          <nav>
+            <Link to="/">Nation Park List </Link>
+          </nav>
+          <Route exact path="/" render={() => (
+            <ParkList parkList={this.state.parkList} />
+          )} />
+          <Route path="/park/:id" render={(routerProps) => (
+            <ParkDetail 
+            parkList={this.state.parkList}
+            {...routerProps}
+            />
+          )} />
+        </div>
+    );
+  }
 }
 
 export default App;
