114127b7aa310da08f1d5132c3bc524f268a8c40
bruce-h <bdhay@protonmail.com>
2021-02-21 12:55:08 -0600

    API pulling data

--- src/App.js
+++ src/App.js
@@ -3,48 +3,35 @@ import './App.css';
 
 import {Route, Link} from 'react-router-dom';
 import axios from 'axios';
-import parkData from './data/parks.json';
-import ParkList from './ParkList';
-import ParkDetail from './ParkDetail';
 
-
+import ArticleList from './ArticleList';
+import ArticleDetail from './ArticleDetail';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      parkList:[]
+      articleList:[]
     }
   }
 
-
   componentDidMount = async () => {
-    const parkData = await axios.get("https://spaceflightnewsapi.net/api/v2/articles");
+    const articleData = await axios.get("https://spaceflightnewsapi.net/api/v2/articles");
     this.setState({
-      parkList: parkData.data.data
+      articleList: articleData.data
     })
   
   }
-
   render() {
+    console.log(this.state.articleList);
     return (
         <div>
-          <nav>
-            <Link to="/">Nation Park List </Link>
-          </nav>
-          <Route exact path="/" render={() => (
-            <ArticleList parkList={this.state.parkList} />
-          )} />
-          <Route path="/park/:id" render={(routerProps) => (
-            <ParkDetail 
-            parkList={this.state.parkList}
-            {...routerProps}
-            />
-          )} />
+          <h1>App.js</h1>
         </div>
     );
   }
 }
+
 
 export default App;
