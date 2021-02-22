92690985ffa93dcac68c98f2e7cdc9df4cb6278b
nealpoppe <npoppe@gmail.com>
2021-02-22 09:30:12 -0600

    api fix

--- src/App.js
+++ src/App.js
@@ -12,16 +12,18 @@ class App extends Component {
     super(props);
 
     this.state = {
-      articleList:[]
+      articleList:[],
+      apiDataLoaded: false
     }
   }
 
   componentDidMount = async () => {
     const articleData = await axios.get("https://spaceflightnewsapi.net/api/v2/articles");
     this.setState({
-      articleList: articleData.data
+      articleList: articleData.data,
+      apiDataLoaded: true
     })
-  
+
   }
   render() {
     console.log(this.state.articleList);
@@ -30,15 +32,23 @@ class App extends Component {
           <nav>
             <Link to="/">Space News List</Link>
           </nav>
-          <Route exact path="/" render={() => (
-            <ArticleList articleList={this.state.articleList} />
-          )} />
-          <Route path="/article/:id" render={(routerProps) => (
-          <ArticleDetail 
-          articleList={this.state.articleList}
-          {...routerProps}
-          />
-          )} />
+          <div>
+            {this.state.apiDataLoaded ? (
+              <div>
+                <Route exact path="/" render={() => (
+                  <ArticleList articleList={this.state.articleList} />
+                )} />
+                <Route path="/article/:id" render={(routerProps) => (
+                <ArticleDetail
+                articleList={this.state.articleList}
+                {...routerProps}
+                />
+                )} />
+              </div>
+              ) :
+              <p>Article Data Not Loaded</p>
+            }
+          </div>
         </div>
     );
   }
