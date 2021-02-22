ea1180347cc0656835c769f30ee3e8446a1a8993
epioletti <PiolettiElizabethL@JohnDeere.com>
2021-02-22 12:36:44 -0800

    possibly checked something into master

--- src/ArticleDetail.js
+++ src/ArticleDetail.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
+// maybe include more info
 const ArticleDetail = props => {
     const foundArticle = props.articleList.find(article => {
         return article.id === props.match.params.id;
     })
-    console.log(foundArticle)
+    // console.log(foundArticle)
     return (
-        // <h2>Founnd id {foundArticle} </h2>
         <div>
-         <h2>{foundArticle.title}</h2>
-         <h2>{foundArticle.summary}</h2>
-         <img src={foundArticle.imageUrl} alt="Article" />
+            <div className="image-text-wrapper">
+                <img src={foundArticle.imageUrl} alt="Article" />
+                <h2 className="image-text">{foundArticle.title}</h2>
+                </div>
+            <div className="description">
+                <h2>{foundArticle.summary}</h2>
+            </div>
         </div>
 
         
