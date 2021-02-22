20e7558dfda2a17dae9cdee5735bb2066678c182
nealpoppe <npoppe@gmail.com>
2021-02-22 10:09:21 -0600

    Article List

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ArticleList = props => {
   console.log(props.articleList);
   return(
     <div>
-      <h1>On the Article List page</h1>
+      {props.articleList.map(article => (
+        <div className="arrange-list" key={article.id}>
+          <div className="list-image-text-wrapper">
+            <Link to={`/article/${article.id}`}>
+              <img src={article.imageUrl} alt="image" />
+              <h2 className="list-image-text">{article.title}</h2>
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
