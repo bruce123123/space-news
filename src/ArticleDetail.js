0882e1c1c4e88a5697adb4a36d181f9b1830486e
epioletti <PiolettiElizabethL@JohnDeere.com>
2021-02-22 13:33:55 -0800

    included some css for title and more button

--- src/ArticleDetail.js
+++ src/ArticleDetail.js
@@ -8,12 +8,15 @@ const ArticleDetail = props => {
     return (
         <div>
             <div className="image-text-wrapper">
-                <img src={foundArticle.imageUrl} alt="Article" />
                 <h2 className="image-text">{foundArticle.title}</h2>
-                </div>
+                <img src={foundArticle.imageUrl} alt="Article" />
+            </div>
             <div className="description">
                 <h2>{foundArticle.summary}</h2>
             </div>
+            <a href={foundArticle.url}>
+              <button className="myButton">Read More</button>
+            </a>          
         </div>
 
         
