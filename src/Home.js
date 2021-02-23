679f9c09081aeb2ccffcd979b985823840b18fa0
nealpoppe <npoppe@gmail.com>
2021-02-23 16:01:13 -0600

    working on home page

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Home = props => {
+  return(
+    <div id='stars'>
+      <div id='stars2'>
+        <div id='stars3'>
+          <div id='title'>
+            <span>
+            Space News
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Home;
