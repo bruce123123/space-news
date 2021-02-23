ec5999211a317f4926988b54a3014d32653722a3
bruce-h <bdhay@protonmail.com>
2021-02-23 09:17:15 -0600

    title page and css adjustment of nav

---
+++ src/TitlePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const TitlePage = props => {
+    return(
+
+        <div>
+            <h1>Title Page</h1>
+        </div>
+    )
+}
+
+
+
+
+export default TitlePage;
